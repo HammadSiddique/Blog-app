55efe40ff44308c0ba997003ccfb150de8922756
HammadSiddique <hammad.siddique01@gmail.com>
2022-08-26 00:12:53 +0500

    Update show html for user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :likes
   has_many :comments
 
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   def update_posts_counter
-    author.increment!(:posts_counter)
+    user.increment!(:posts_counter)
   end
 
   def recent_comments
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ puts "Seeding..."
 first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
 
-post1 = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-post2 = Post.create(author: first_user, title: 'World', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
-post3 = Post.create(author: first_user, title: 'Coding', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
-post4 = Post.create(author: second_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-post5 = Post.create(author: second_user, title: 'World', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+post1 = Post.create(user: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+post2 = Post.create(user: first_user, title: 'World', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+post3 = Post.create(user: first_user, title: 'Coding', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+post4 = Post.create(user: second_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+post5 = Post.create(user: second_user, title: 'World', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
 
 
 Comment.create(post: post1, user: second_user, text: 'Hi Tom!' )
