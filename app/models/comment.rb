16049ab9ebadf8dbb935f6f6204b876891b7f537
HammadSiddique <hammad.siddique01@gmail.com>
2022-08-26 21:29:34 +0500

    Update view for a single post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_save :update_comments_counter
-  
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
