4997ba1a468aae046a112e1ad698c0abee19ad69
HammadSiddique <hammad.siddique01@gmail.com>
2022-08-28 01:04:11 +0500

    Register new likes for the posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.create
+    @like.user = current_user
+    @like.post = @post
+    if @like.save
+      flash[:success] = 'Like added successfully!'
+      redirect_to user_posts_path(current_user, @post)
+    else
+      flash.now[:error] = 'Something unexpected happened, like could not be added.'
+    end
+  end
 end
