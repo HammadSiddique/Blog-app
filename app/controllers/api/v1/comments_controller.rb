f303edf3965cd867e3bddd582f8a160f441899d4
GOlivierNation <ogasominali@gmail.com>
2022-09-03 14:49:56 -0400

    Create json format for post's comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::CommentsController < ApplicationController
+    protect_from_forgery with: :null_session
+  
+    def index
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+      @comments = @post.comments
+      render json: @comments
+    end
+  
+    def create
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+      @comment = @post.comments.create(text: comment_params[:text], user_id: @user.id, post_id: @post.id)
+  
+      if @comment.save
+        render json: @comment, status: :created
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
+  
